e96bf186b4d815d8d0a9d34af8359fa44e3d3bf8
R-Sai-Chirag <saichirag355@gmail.com>
2025-05-16 21:52:32 +0530

    data ingestion

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,43 @@
+import os
+import sys
+import pandas as pd
+
+from src.exception import coustom_execption
+from dataclasses import dataclass
+from sklearn.model_selection import train_test_split
+
+
+@dataclass
+class dataingestionconfig:
+    train_data_path: str=os.path.join("artifacts","train.csv")
+    test_data_path: str=os.path.join("artifacts","test.csv")
+    raw_data_path: str=os.path.join("artifacts","data_csv")
+
+class dataingestion:
+    def __init__(self):
+        self.ingestion_config=dataingestionconfig()
+
+    def initiate_data_ingestion(self):
+        try:
+          df=pd.read_csv("stud.csv")
+          os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+
+          df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+
+          train_set,test_set=train_test_split(df,random_state=10,test_size=0.25)
+
+          train_set.to_csv(self.ingestion_config.train_data_path,index=False)
+
+          test_set.to_csv(self.ingestion_config.test_data_path,index=False)
+
+          return(
+              self.ingestion_config.train_data_path,
+              self.ingestion_config.test_data_path
+          )
+        
+        except Exception as e:
+            raise coustom_execption(e,sys)
+        
+if __name__=="__main__":
+    obj=dataingestion()
+    obj.initiate_data_ingestion()
